1e8cc0216caee21ebfa16a0318a196b108bb9b91
HenryCarrillo <65102332+henrycarrillo34@users.noreply.github.com>
2024-06-10 00:57:46 -0500

    Comando AT

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
+/*
+MODULO SIM808
+*/
 #include <Arduino.h>
 #include <ModbusMaster.h>
 #include <ArduinoJson.h> 
@@ -40,6 +43,8 @@ uint16_t energia;
 //#define MAX485_DE 12
 #define MAX485_DE 32
 #define led_esp32 2
+// ---- SOFTWARE TRIGGER GPRS ----
+int PowerPin = 23; //prendido de placa SIM808
 
 //*************************
 //       JSON
@@ -53,14 +58,48 @@ int16_t potencia;
 int16_t voltaje;
 int16_t energia; 
 
-String servidor = "http://869c-38-25-16-250.sa.ngrok.io/api/lecturas/crearPorSerie/IM001";
+String servidor = "http://54.83.202.208:8080/api/v1/module-data/8";
 
-//***********************
-//   GSM
-void comandosAT()
+//**************************
+//        FUNCIONES
+//**************************
+void software_trigger(){
+  pinMode(PowerPin, OUTPUT);
+  //TURN OFF the GSM module -software switch instead Power Key
+  digitalWrite(PowerPin, LOW);
+  delay (2000);
+  digitalWrite (PowerPin, HIGH);  
+  delay (10000); //delay set for 10 seconds
+}
+//***********************//
+//       GSM
+//***********************//
+void comandos_red_AT(){
+  Serial2.println("AT"); /* Check Communication */
+  delay(500);
+  Serial2.println("AT+CSTT=\"https://wap.movistar.pe\",\"movistar@wap\",\"movistar\"");
+  delay(500);
+  Serial2.println("AT+SAPBR=3,1,\"Contype\",\"GPRS\"");
+  delay(500);
+  Serial2.println("AT+SAPBR=3,1,\"APN\",\"movistar.pe\"");
+  delay(500);
+  Serial2.println("AT+SAPBR=3,1,\"USER\",\"movistar@datos\"");
+  delay(500);
+  Serial2.println("AT+SAPBR=3,1,\"PWD\",\"Movistar\"");
+  delay(500);
+  Serial2.println("AT+SAPBR=1,1");
+  delay(500);
+  Serial2.println("AT+SAPBR=2,1");
+  delay(500);
+}
+
+
+void comandosAT_JSON()
 {
   Serial2.println("AT+HTTPINIT");
   delay(50);
+  Serial2.println("AT+HTTPPARA=\"CID\",1");
+  delay(500);
   Serial2.println("AT+HTTPPARA=\"URL\",\""+servidor+"\""); //Server address
   delay(25);
   Serial2.println("AT+HTTPPARA=\"CONTENT\",\"application/json\"");
@@ -72,8 +111,12 @@ void comandosAT()
   json="";
   Serial2.println("AT+HTTPACTION=1");
   delay(25);
+  Serial2.println("AT+HTTPREAD");
+  delay(40);
   Serial2.println("AT+HTTPTERM");
   delay(50);
+  Serial2.println("AT+SAPBR=0,1");
+  delay(1000);
   while(Serial2.available()!=0){/* If data is available on serial port */
     //Serial.write(char (Serial.read()));
     String cadena = Serial2.readString() ; 
@@ -81,28 +124,41 @@ void comandosAT()
   }
 }
 
+
 void setup() {
   //TERMINAL RX0-TX0 -> SIM808
   Serial.begin(9600);
   //TERMINAL RX2-TX2 -> WATIMETRO
-  Serial2.begin(9600, SERIAL_8N1, RXp2, TXp2); //Dispositivo de bajada
+  Serial2.begin(9600, SERIAL_8N1, RXp2, TXp2); 
   //TERMINAL RX3-TX3 -> TERMINAL
   U3_Debug.begin(9600, SWSERIAL_8N1, RX3, TX3, false, 256); // BADURATE del software serial de comunicacion
   // high speed half duplex, turn off interrupts during tx
+  pinMode(LED, OUTPUT);
   U3_Debug.enableIntTx(false);
   pinMode(MAX485_DE, OUTPUT);
   pinMode(led_esp32,OUTPUT);
+  U3_Debug.enableIntTx(false);
+  software_trigger();
+  //esperar
+  delay(10000);
+  comandos_red_AT();
 }
 
 
 
 
 void loop() {
+  if ( Serial2.available() > 0 ){
+
+
+
+  }
   //cadena_amb_json = cadena_amb ; 
   doc["corriente"] = corriente;
   doc["voltaje"] = voltaje;
   doc["potencia"] = potencia;
   doc["energia"] = potencia;
   serializeJson(doc, json); 
-  comandosAT();
+  comandos_red_AT();
+  comandosAT_JSON();
 }
